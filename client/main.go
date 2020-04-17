fa51a60115276e70a073c9ef47b34d134bdbef46
Eduardo Lima <eduardolima@tupe-009090.local>
2020-04-17 00:52:20 -0300

    server and client

---
+++ client/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/EduardoLima806/grpc-hello-go/pb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	connection, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+
+	defer connection.Close()
+
+	client := pb.NewHelloServiceClient(connection)
+
+	Hello(err, client)
+}
+
+func Hello(err error, client pb.HelloServiceClient) {
+
+	request := pb.HelloRequest{
+		Name: "Eduardo Lima",
+	}
+
+	res, err := client.Hello(context.Background(), &request)
+
+	if err != nil {
+		log.Fatalf("Erro during the execution: %v", err)
+	}
+
+	log.Println(res.Msg)
+}
--- server/main.go
+++ server/main.go
@@ -1,13 +1,25 @@
-package server
+package main
 
 import (
+	"context"
 	"log"
 	"net"
 
+	"github.com/EduardoLima806/grpc-hello-go/pb"
 	"google.golang.org/grpc"
 )
 
 type server struct{}
+
+func (*server) Hello(ctx context.Context, request *pb.HelloRequest) (*pb.HelloResponse, error) {
+	result := "Hello " + request.GetName()
+
+	res := &pb.HelloResponse{
+		Msg: result,
+	}
+
+	return res, nil
+}
 
 func main() {
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
@@ -16,4 +28,9 @@ func main() {
 	}
 
 	grpcServer := grpc.NewServer()
+	pb.RegisterHelloServiceServer(grpcServer, &server{})
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to server %v", err)
+	}
 }
